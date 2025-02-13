e3c14c80d4eebef90a6d274aa03dd82dd1cd5a87
shoc71 <supersonicwind6@gmail.com>
2025-02-13 12:56:45 -0500

    re-organization

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 // imports
 import express from 'express';
 import { getCity } from './server.route.js';
+import dotenv from 'dotenv';
 
 // middleware
+dotenv.config();
 const route = express();
+const PORT = process.env.PORT
 route.use(express.json());
 
 route.get('/', getCity);
 
-route.listen(3000, () => {
-    console.log('Server is running on port 3000');
+route.listen(PORT, () => {
+    console.log('Server is running on port ', PORT);
 });
